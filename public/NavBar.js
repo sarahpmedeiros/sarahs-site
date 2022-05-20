3fe3aed5f06447b2e230fb59df75d2131064a11e
Sarah Medeiros <smedeiros@wayfair.com>
2022-05-20 16:26:50 -0400

    Add structure, texts, photos

---
+++ public/NavBar.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBlCYzBzH9QdUFSeKK-QQoDFQDoAgNWgj4",
+    authDomain: "sarahs-site.firebaseapp.com",
+    projectId: "sarahs-site",
+    storageBucket: "sarahs-site.appspot.com",
+    messagingSenderId: "363594158853",
+    appId: "1:363594158853:web:748dc95e097a58ac291d6c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
